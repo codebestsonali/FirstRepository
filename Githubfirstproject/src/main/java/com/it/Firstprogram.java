680c3607b7a19076fd53cefb8c4af39967ac2b0f
AnilMohandu <Anil.Mohandu@macys.com>
2018-10-28 12:36:19 +0530

    SecondCommit into GHF by Anil

--- Githubfirstproject/src/main/java/com/it/Firstprogram.java
+++ Githubfirstproject/src/main/java/com/it/Firstprogram.java
@@ -4,4 +4,8 @@ public class Firstprogram {
 	public static void main(String[] args) {
 		System.out.println("hello god");
 	}
+	
+	static void m1() {
+	  System.out.println("hi sonali how are you????");	
+	}
 }
